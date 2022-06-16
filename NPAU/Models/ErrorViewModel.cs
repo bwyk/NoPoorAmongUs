c3ca8d8d9684d99ee3edff3f9ec8f6296964dd8e
aaronwebb1 <aaronwebb1@mail.weber.edu>
2022-06-16 12:31:52 -0600

    Add project files.

---
+++ NPAU/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NPAU.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
