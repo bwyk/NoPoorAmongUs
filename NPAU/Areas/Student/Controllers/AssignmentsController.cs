3c579ff81142415dbfa3c7dc69aec103d5ae511e
robertcook2 <46454069+robertcook2@users.noreply.github.com>
2022-06-21 14:14:35 -0600

    Added Controllers

---
+++ NPAU/Areas/Student/Controllers/AssignmentsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NPAU.Controllers
+{
+    [Area("Student")]
+    public class AssignmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
