41f793e44798b75de0d5781492e01b54adb33874
kevinmclennan <kevin.mclennan@orbitonline.com>
2022-07-05 19:33:47 -0600

    Adjusted table width and link under Admin for students will show All students.

--- NPAU/wwwroot/js/studentFilter.js
+++ NPAU/wwwroot/js/studentFilter.js
@@ -31,34 +31,36 @@ function loadDataTable(status) {
             "url": "/Applicant/Manage/GetAll?status=" + status
         },
         "columns": [
-            { "data": "status", "width": "%10" },
-            { "data": "fullName", "width": "10%" },
+            { "data": "status", "width": "%14" },
+            { "data": "fullName", "width": "14%" },
             {
-                "data": "birthday", "width": "10%",
+                "data": "birthday", "width": "14%",
                 "render": DataTable.render.date()
             },
-            { "data": "address", "width": "10%" },
-            { "data": "englishLevel", "width": "10%" },
-            { "data": "computerLevel", "width": "10%" },
+            { "data": "address", "width": "14%" },
+            { "data": "englishLevel", "width": "14%" },
+            { "data": "computerLevel", "width": "14%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `
-                        <div class="w-75 btn-group" role="group">
-                        <a href="/Applicant/Manage/Details?id=${data}"
-                        class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Details</a>
-                        </div>
-                        <div class="w-75 btn-group" role="group">
-                        <a href="/Applicant/Manage/Edit?id=${data}"
-                        class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
-                        </div>
-                        <div class="w-75 btn-group" role="group">
-                        <a href="/Applicant/Manage/Delete?id=${data}"
-                        class="btn btn-danger mx-2"> <i class="bi bi-pencil-square"></i>Delete</a>
+                        <div class="btn-group" role="group">
+                            <div class="w-75 btn-group" role="group">
+                            <a href="/Applicant/Manage/Details?id=${data}"
+                            class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Details</a>
+                            </div>
+                            <div class="w-75 btn-group" role="group">
+                            <a href="/Applicant/Manage/Edit?id=${data}"
+                            class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
+                            </div>
+                            <div class="w-75 btn-group" role="group">
+                            <a href="/Applicant/Manage/Delete?id=${data}"
+                            class="btn btn-danger mx-2"> <i class="bi bi-pencil-square"></i>Delete</a>
+                            </div>
                         </div>
                         `
                 },
-                "width": "10%"
+                "width": "14%"
             }
         ]
     });
