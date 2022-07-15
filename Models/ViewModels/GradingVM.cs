0cd5e3f3c8af27db7ce8833a4d93c10f632810cc
Brendon Wykstra <brendonwykstra@mail.weber.edu>
2022-07-15 16:12:56 -0600

    First commit. Incomplete.

---
+++ Models/ViewModels/GradingVM.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Models.Academic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Models.ViewModels
+{
+    public class GradingVM
+    {
+
+        public Grade Grade { get; set; }
+        [ValidateNever]
+        public IEnumerable<Student> StudentList { get; set; }
+        
+        
+    }
+}
---
+++ NPAU/Areas/Student/Controllers/GradingController.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.Academic;
+using Models.ViewModels;
+
+namespace NPAU.Areas.Student.Controllers
+{
+    [Area("Student")]
+    public class GradingController : Controller
+    {
+        
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GradingController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult CourseSelect()
+        {
+            IEnumerable<Course> courseList = _unitOfWork.Course.GetAll();
+            return View(courseList);
+        }
+
+        public IActionResult AssessmentSelect(int courseId)
+        {
+            IEnumerable<Assessment> assessmentList = _unitOfWork.Assessment.GetAll(a => a.CourseId == courseId);
+            return View(assessmentList);
+        }
+
+        public IActionResult GradingTable(int assessmentId)
+        {
+            Assessment targetAssessment = _unitOfWork.Assessment.GetFirstOrDefault(a => a.Id == assessmentId);
+            GradingVM gradingVM = new GradingVM();           
+            
+            IEnumerable<CourseEnrollment> ceList = _unitOfWork.CourseEnrollment.GetAll((cE => cE.CourseSession.CourseId == targetAssessment.CourseId), includeProperties: "Student");
+            foreach(CourseEnrollment cE in ceList)
+            {
+                if (!gradingVM.StudentList.Contains(cE.Student)) {
+                    gradingVM.StudentList.Append(cE.Student);
+                }
+            }
+            return View(gradingVM);
+        }
+
+        #region API CALLS
+        public IActionResult GetAll()
+        {
+            var gradingList = _unitOfWork.Grade.GetAll(includeProperties: "Assessment,CourseEnrollment,Course");
+            return Json(new { data = gradingList });
+        }
+        #endregion
+    }
+
+
+}
