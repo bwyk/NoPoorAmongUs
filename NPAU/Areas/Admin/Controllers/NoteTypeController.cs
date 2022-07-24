7b12a3a75a27f278ffaf92bfdf4b09839ba3eaf3
kevinmclennan <kevin.mclennan@orbitonline.com>
2022-07-23 22:03:14 -0600

    CRUD for Roles and Notes working.  Have issue with updating name and roles at the same time.

--- NPAU/Areas/Admin/Controllers/NoteTypeController.cs
+++ NPAU/Areas/Admin/Controllers/NoteTypeController.cs
@@ -22,7 +22,6 @@ namespace NPAU.Areas.Admin.Controllers
         }
         public ViewResult Index()
         {
-            //This currently works to fill the html with type and list of roles.  They are strings though.
             List<string> noteTypes = _unitOfWork.NoteType.GetAll().Select(n => n.Type).Distinct().ToList();
             Dictionary<string, List<string>> allData = new Dictionary<string, List<string>>();
 
@@ -40,8 +39,8 @@ namespace NPAU.Areas.Admin.Controllers
             return View(noteTypeVM);
         }
 
-
-        public IActionResult Upsert(int? id)
+        [HttpGet]
+        public IActionResult Upsert(string? type)
         {
             NoteTypeVM noteTypeVM = new NoteTypeVM()
             {
@@ -49,13 +48,24 @@ namespace NPAU.Areas.Admin.Controllers
                 RoleList = _roleManager.Roles.Select(r => r.Name).ToList()
             };
 
-            if (id == null || id == 0)
+            if (type == null)
             {
                 return View(noteTypeVM);
             }
             else
             {
-                noteTypeVM.NoteType = _unitOfWork.NoteType.GetFirstOrDefault(u => u.Id == id, includeProperties: "Role");
+                List<string> noteTypes = _unitOfWork.NoteType.GetAll().Select(n => n.Type).Distinct().ToList();
+                Dictionary<string, List<string>> allData = new Dictionary<string, List<string>>();
+
+                foreach (var nt in noteTypes)
+                {
+                    List<string> rolesToAdd = new List<string>();
+                    rolesToAdd = _unitOfWork.NoteType.GetAll(u => u.Type == nt, includeProperties: "Role").Select(n => n.Role.Name).ToList();
+                    allData.Add(nt, rolesToAdd);
+                }
+
+                noteTypeVM.NoteRoles = allData;
+                noteTypeVM.NoteType = _unitOfWork.NoteType.GetFirstOrDefault(t => t.Type == type);
                 noteTypeVM.RoleList = _roleManager.Roles.Select(r => r.Name).ToList();
 
                 return View(noteTypeVM);
@@ -70,18 +80,75 @@ namespace NPAU.Areas.Admin.Controllers
             var roleIds = new List<string>();
             List<NoteType> notetypes = new List<NoteType>();
 
-            foreach (var item in checkedRoles)
+            //New NoteType
+            if (obj.NoteType.Id == 0)
             {
-                obj.NoteType.Role = _roleManager.Roles.FirstOrDefault(r => r.Name == item);
-                NoteType nt = new NoteType();
-                nt.Type = obj.NoteType.Type;
-                nt.Role = obj.NoteType.Role;
-                notetypes.Add(nt);
-            }
+                foreach (var item in checkedRoles)
+                {
+                    obj.NoteType.Role = _roleManager.Roles.FirstOrDefault(r => r.Name == item);
+                    NoteType nt = new NoteType();
+                    nt.Type = obj.NoteType.Type;
+                    nt.Role = obj.NoteType.Role;
+                    notetypes.Add(nt);
+                }
 
-            _unitOfWork.NoteType.AddRange(notetypes);
-            _unitOfWork.Save();
-            return RedirectToAction("Index");
+                _unitOfWork.NoteType.AddRange(notetypes);
+                _unitOfWork.Save();
+                TempData["success"] = "Note Type created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //Update Notetype
+                //Find what the currently saved Note Type Roles are.
+                obj.RoleList = _unitOfWork.NoteType.GetAll(nt => nt.Type == obj.NoteType.Type, includeProperties: "Role").Select(n => n.Role.Name).ToList();
+                var oldTypeName = _unitOfWork.NoteType.GetFirstOrDefault(nt => nt.Id == obj.NoteType.Id, includeProperties: "Role");
+                List<string> oldRoles = _unitOfWork.NoteType.GetAll(nt => nt.Type == oldTypeName.Type, includeProperties: "Role").Select(n => n.Role.Name).ToList();
+                oldRoles.Sort();
+                checkedRoles.Sort();
+
+                //Compare old checked roles to the new incoming checked roles.
+                if (oldRoles.SequenceEqual(checkedRoles))
+                {
+                    //Roles didn't change.  Update NoteType Type
+                    List<NoteType> noteTypeUpdates = _unitOfWork.NoteType.GetAll(nt => nt.Type == oldTypeName.Type).ToList();
+                    foreach (NoteType nt in noteTypeUpdates)
+                    {
+                        nt.Type = obj.NoteType.Type;
+                    }
+
+                    _unitOfWork.NoteType.UpdateRange(noteTypeUpdates);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Note Type editted successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    var list1 = oldRoles.Except(checkedRoles); //removed
+                    var list2 = checkedRoles.Except(oldRoles); //added
+                    List<NoteType> rolesToRemove = new List<NoteType>();
+                    List<NoteType> rolesToAdd = new List<NoteType>();
+                    foreach (string s in oldRoles.Except(checkedRoles))
+                    {
+                        NoteType remove = _unitOfWork.NoteType.GetFirstOrDefault(nt => nt.Type == oldTypeName.Type && nt.Role.Name == s, includeProperties: "Role");
+                        rolesToRemove.Add(remove);
+                    }
+                    foreach (string s in checkedRoles.Except(oldRoles))
+                    {
+                        NoteType add = new NoteType();
+                        add.Type = obj.NoteType.Type;
+                        add.Role = _roleManager.Roles.FirstOrDefault(r => r.Name == s);
+                        rolesToAdd.Add(add);
+                    }
+
+                    _unitOfWork.NoteType.AddRange(rolesToAdd);
+                    _unitOfWork.NoteType.RemoveRange(rolesToRemove);
+                    _unitOfWork.Save();
+
+                    TempData["success"] = "Note Type updated successfully";
+                    return RedirectToAction("Index");
+                }
+            }
         }
 
         [HttpGet]
