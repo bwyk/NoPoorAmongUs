fccb09662291e06b04ce4f3d17655fc1417edab6
Brendon Wykstra <brendonwykstra@mail.weber.edu>
2022-07-30 11:49:02 -0600

    Working towards seeing grades by course->student. Returning null query on courseSession.

---
+++ Models/ViewModels/StudentAssessmentGradesVM.cs
@@ -0,0 +1,18 @@
+﻿using Models.Academic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class StudentAssessmentGradesVM
+    {
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+        public List<Assessment> Assessments { get; set; }
+        public List<Grade> Grades { get; set; }
+        
+    }
+}
