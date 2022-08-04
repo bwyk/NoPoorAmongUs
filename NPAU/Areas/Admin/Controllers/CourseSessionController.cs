ff7bfb7ec93c7680a72ba748a7951cdc037f0a06
aaronwebb1 <aaronwebb1@mail.weber.edu>
2022-08-03 18:00:04 -0600

    Changed to Course Session to upsert, and changed coourses

--- NPAU/Areas/Admin/Controllers/CourseSessionController.cs
+++ NPAU/Areas/Admin/Controllers/CourseSessionController.cs
@@ -21,57 +21,56 @@ public class CourseSessionController : Controller
         IEnumerable<CourseSession> objSessionList = _unitOfWork.CourseSession.GetAll();
         return View(objSessionList);
     }
-
-
-    [HttpGet]
-    public ViewResult Create()
+    public async Task<IActionResult> UpsertAsync(int? id)
     {
-        return View();
+        CourseSessionVM courseSessionVM = new()
+        {
+            CourseSession = new(),
+            CourseList = _unitOfWork.Course.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }),
+        };
+
+        if (id == null || id == 0)
+        {
+            return View(courseSessionVM);
+        }
+        else
+        {
+            courseSessionVM.CourseSession = _unitOfWork.CourseSession.GetFirstOrDefault(u => u.Id == id);
+            return View(courseSessionVM);
+
+        }
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Create(CourseSession obj)
-
+    public async Task<IActionResult> UpsertAsync(CourseSessionVM obj)
     {
         if (ModelState.IsValid)
         {
-            _unitOfWork.CourseSession.Add(obj);
+            if (obj.CourseSession.Id == 0)
+            {
+                _unitOfWork.CourseSession.Add(obj.CourseSession);
+                TempData["success"] = "Course Created Successfully";
+
+            }
+            else
+            {
+                _unitOfWork.CourseSession.Update(obj.CourseSession);
+                TempData["success"] = "Course Updated Successfully";
+
+            }
             _unitOfWork.Save();
-            TempData["success"] = "Course Session added successfully!";
             return RedirectToAction("Index");
         }
-        return View(obj);
-    }
-
-    [HttpGet]
-    public IActionResult Edit(int? id)
-    {
-        if (id == null || id == 0)
+        obj.CourseList = _unitOfWork.Course.GetAll().Select(i => new SelectListItem
         {
-            return NotFound();
-        }
-
-        var CourseSessionFromDb = _unitOfWork.CourseSession.GetFirstOrDefault(t => t.Id == id);
-
-        if (CourseSessionFromDb == null)
-        {
-            return NotFound();
-        }
-
-        return View(CourseSessionFromDb);
-    }
-
-    [HttpPost, ValidateAntiForgeryToken]
-    public IActionResult Edit(CourseSession obj)
-    {
-        if (ModelState.IsValid)
-        {
-            _unitOfWork.CourseSession.Update(obj);
-            _unitOfWork.Save(); 
-            TempData["success"] = "Course Session updated successfully!";
-            return RedirectToAction("Index");
-        }
+            Text = i.Name,
+            Value = i.Id.ToString()
+        });
 
         return View(obj);
     }
@@ -102,7 +101,19 @@ public class CourseSessionController : Controller
         {
             return NotFound();
         }
-
+        var courseEnrollments = _unitOfWork.CourseEnrollment.GetAll();
+        foreach (CourseEnrollment ce in courseEnrollments)
+        {
+            if(ce.CourseSessionId == id)
+            {
+                var grade = _unitOfWork.Grade.GetAll().Where(c => ce.CourseSessionId == id);
+                foreach (Grade g in grade)
+                {
+                    _unitOfWork.Grade.Remove(g);
+                }
+                _unitOfWork.CourseEnrollment.Remove(ce);
+            }
+        }
         _unitOfWork.CourseSession.Remove(obj);
         _unitOfWork.Save();
         TempData["success"] = "Course Session deleted successfully.";
